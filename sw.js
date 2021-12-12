2c122b6ba45290fb4266de87838bf0d6a7bf51be
kutup tilkisi <89846189+kutuptilkisi@users.noreply.github.com>
2021-12-12 20:08:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-APP.get('/manifest.json', function(req, res) {
-    res.append('Content-Type', 'text/javascript');
-    res.sendFile(path.join(root, "manifest.json"));
-});
-
-APP.get('/sw.js', function(req, res) {
-    res.append('Content-Type', 'text/javascript');
-    res.sendFile(path.join(root, "sw.js"));
+console.log("sw loaded")
+self.addEventListener('fetch', function(event) {
+  // Perform some task
 });
