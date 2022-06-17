ae5d2548be5241f6a697a6abef1f0dff06f18305
kutup tilkisi <89846189+kutuptilkisi@users.noreply.github.com>
2022-06-17 19:55:07 +0300

    Update bildirim.js

--- js/bildirim.js
+++ js/bildirim.js
@@ -1,12 +1,9 @@
 if (Notification.permission !== 'denied') {
-    // daha önce bildirimlere onay verilmediyse önce onay istenecek
 	Notification.requestPermission(function (permission) {
-	// Kullanıcı onaylamadı ise tekrar soralım
 	if (permission === "granted") {
-	    // onaylarsa bilgilendirme bildirimi gönderilecek
-		var notification = new Notification('Kodaktuel.com', {
-		body: 'Artık Kodaktuel.com yazılarından anında haberdar olacaksınız. Teşekkür ederiz.', 
-		icon: 'http://kodaktuel.com//upload/dosya/kodaktuellogo_15798a891749d0.png'
+		var notification = new Notification('kutuptilkisi.github.io', {
+		body: 'merhaba!', 
+		icon: 'assets/favicon.png'
 		});
 	}
     });
